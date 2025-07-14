69bbe8617875b451479389d8b98916bfa4d24309
nurikhsanGIT <nurikhsan@students.amikom.ac.id>
2025-07-14 15:40:55 +0700

    update terbaruuu

--- interactive-cv/frontend/vite.config.js
+++ interactive-cv/frontend/vite.config.js
@@ -1,10 +1,8 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { fileURLToPath, URL } from 'node:url'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -15,4 +13,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  build: {
+    outDir: 'dist'
+  }
 })
